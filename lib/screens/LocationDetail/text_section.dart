e83f0591e3c3518a33b091fe5c200f93f097849f
paulzay <paulotieno2@gmail.com>
2021-04-23 10:13:23 +0300

    implement text component/widget

---
+++ lib/screens/LocationDetail/text_section.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class TextSection extends StatelessWidget {
+  final String _title;
+  final String _body;
+
+  TextSection(this._title, this._body);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Text(_title),
+        Text(_body),
+      ]);
+  }
+}
