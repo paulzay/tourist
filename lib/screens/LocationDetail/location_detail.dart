e1e3fc8bdf59c687b94fed27848b449d6642b311
paulzay <paulotieno2@gmail.com>
2021-04-23 17:06:29 +0300

    wrap up location detail feature

--- lib/screens/LocationDetail/location_detail.dart
+++ lib/screens/LocationDetail/location_detail.dart
@@ -1,24 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:tourist/models/location.dart';
 import 'image_banner.dart';
 import 'text_section.dart';
 
 class LocationDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final locations = Location.fetchAll();
+    final location = locations.first;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Zay Tours'),
+        title: Text(location.name),
       ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          ImageBanner("assets/images/dancer.jpg"),
-          TextSection('title1', 'paragraph1'),
-          TextSection('title2', 'paragraph2'),
-          TextSection('title3', 'paragraph3'),
-        ],
+          ImageBanner(location.imagePath),
+        ]..addAll(textSections(location))
       ),
     );
+  }
+
+  List<Widget> textSections(Location location) {
+    return location.facts.map((fact) => TextSection(fact.title, fact.text)).toList();
   }
 }
