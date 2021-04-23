d245b1a587a5d4c898e99dc0934fcd222d8e28a3
paulzay <paulotieno2@gmail.com>
2021-04-23 16:44:26 +0300

    add styling

--- lib/screens/LocationDetail/text_section.dart
+++ lib/screens/LocationDetail/text_section.dart
@@ -3,17 +3,22 @@ import 'package:flutter/material.dart';
 class TextSection extends StatelessWidget {
   final String _title;
   final String _body;
+  static const double _hPad = 16.0;
 
   TextSection(this._title, this._body);
 
   @override
   Widget build(BuildContext context) {
     return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Text(_title),
-        Text(_body),
-      ]);
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Container(
+              padding: const EdgeInsets.fromLTRB(_hPad, 32, _hPad, 4),
+              child: Text(_title)),
+          Container(
+              padding: const EdgeInsets.fromLTRB(_hPad, 32, _hPad, 4),
+              child: Text(_body)),
+        ]);
   }
 }
---
+++ lib/styles.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+const LargeTextSize = 26.0;
+const MediumTextSize = 20.0;
+const BodyTextSize = 16.0;
+
+const AppBarTextStyle = TextStyle(
+  fontWeight: FontWeight.w300,
+  fontSize: MediumTextSize,
+  color: Colors.white,
+);
